9d1c15a02bdc72026b8d30206d02c83f72a50486
Ray <rayflyworld@gmail.com>
2022-08-12 15:18:29 +0900

    fix : fix 35 condition

--- Easy/35/run.ts
+++ Easy/35/run.ts
@@ -1,13 +1,10 @@
 function searchInsert(nums: number[], target: number): number {
-    const targetIndex = findInsertPosition(0, nums.length, nums, target);
-    if (nums[targetIndex] === undefined || nums[targetIndex] === target || nums[targetIndex] > target) return targetIndex;
-    if (nums[targetIndex] < target) return targetIndex+1;
+    return findInsertPosition(0, nums.length-1, nums, target);
 };
 
 function findInsertPosition(low: number, high: number, nums: number[], target: number) {
     if (low > high) return low;
     const middle = Math.trunc((low+high)/2);
-    if (nums[middle] === undefined) return high;
     if (target === nums[middle]) return middle;
     if (target > nums[middle]) return findInsertPosition(middle+1, high, nums, target);
     if (target < nums[middle]) return findInsertPosition(low, middle-1, nums, target);
