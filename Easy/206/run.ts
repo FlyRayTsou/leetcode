b80c9e684894c9f0458db24862b7e9ffec76c6b5
Ray <rayflyworld@gmail.com>
2022-09-01 09:02:12 +0900

    add : 206 iterate

--- Easy/206/run.ts
+++ Easy/206/run.ts
@@ -28,3 +28,26 @@ function reverse(preNode:ListNode, curNode: ListNode, answer: ListNode | null) {
     curNode.next = preNode;
     return result;
 }
+
+
+// Runtime: 102 ms, faster than 60.41% of TypeScript online submissions for Reverse Linked List.
+// Memory Usage: 44.7 MB, less than 87.15% of TypeScript online submissions for Reverse Linked List.
+function reverseListIterate(head: ListNode | null): ListNode | null {
+    if (head === null || head.next === null) return head;
+    let stack = [];
+    let pushNode = head;
+    while(pushNode !== null) {
+        stack.push(pushNode.val)
+        pushNode = pushNode.next;
+    }
+    const curValue = stack.pop();
+    let curNode = new ListNode(curValue, null);
+    const answer = curNode;
+    while(stack.length > 0) {
+        const nextNodeValue = stack.pop();
+        const nextNode = new ListNode(nextNodeValue, null);
+        curNode.next = nextNode;
+        curNode = nextNode;
+    }
+    return answer;
+};
