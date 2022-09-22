d52999c236fc0256a1bc28139b10350d8ec9bd11
Ray <rayflyworld@gmail.com>
2022-09-22 21:20:28 +0900

    add : add 3 second solution

--- Medium/3/run.ts
+++ Medium/3/run.ts
@@ -16,3 +16,25 @@ function lengthOfLongestSubstring(s: string): number {
     }
     return max;
 };
+
+
+// Runtime: 514 ms, faster than 21.62% of TypeScript online submissions for Longest Substring Without Repeating Characters.
+// Memory Usage: 49.9 MB, less than 26.03% of TypeScript online submissions for Longest Substring Without Repeating Characters.
+function lengthOfLongestSubstring2(s: string): number {
+    let curString = '';
+    let max = 0;
+    for(let i = 0; i < s.length; i++) {
+        if (curString.length < max && (s.length - i - 1) < max) break;
+        curString = s[i];
+        for (let j = i+1; j < s.length; j++) {
+            if (curString.indexOf(s[j]) === -1) {
+                curString = curString + s[j];
+                continue;
+            }
+            break;
+        }
+        max = Math.max(max, curString.length);
+        
+    }
+    return max;
+};
