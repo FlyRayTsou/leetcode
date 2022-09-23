96024150e5e3fa345e4856560bfc1ee32d0993a8
Ray <rayflyworld@gmail.com>
2022-09-23 17:22:21 +0900

    fix : 78

--- Medium/78/run.ts
+++ Medium/78/run.ts
@@ -17,3 +17,26 @@ function dfs(index: number, isAdd: boolean, nums: number[], result, curResult: n
     }
     return dfs(index+1, false, nums, result, [...curResult]) + dfs(index+1, true, nums, result, [...curResult]);
 }
+
+
+// Runtime: 96 ms, faster than 67.99% of TypeScript online submissions for Subsets.
+// Memory Usage: 44.7 MB, less than 92.99% of TypeScript online submissions for Subsets.
+function subsets2(nums: number[]): number[][] {
+    let result = [];
+    let index = 0;
+    dfs2(index, false, nums, result, [])
+    dfs2(index, true, nums, result, []);
+    return result;
+};
+
+function dfs2(index: number, isAdd: boolean, nums: number[], result, curResult: number[]) {
+    if (isAdd) {
+        curResult.push(nums[index]);
+    }
+    if (index === nums.length-1) {
+        result.push(curResult);
+        return;
+    }
+    dfs2(index+1, false, nums, result, [...curResult])
+    dfs2(index+1, true, nums, result, [...curResult]);
+}
