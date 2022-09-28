dc5144d1032a272947dfdd74453189bc37588795
Ray <rayflyworld@gmail.com>
2022-09-29 00:35:04 +0900

    add : 19

--- Medium/19/run.ts
+++ Medium/19/run.ts
@@ -1,3 +1,22 @@
+// Runtime: 138 ms, faster than 8.23% of TypeScript online submissions for Remove Nth Node From End of List.
+// Memory Usage: 44.7 MB, less than 57.60% of TypeScript online submissions for Remove Nth Node From End of List.
+function removeNthFromEnd2(head: ListNode | null, n: number): ListNode | null {
+    let fastNode = head;
+    let slowNode = head;
+    while (n > 0) {
+        fastNode = fastNode.next;
+        n--;
+    }
+    if (fastNode === null) return head.next;
+    while (fastNode.next != null) {
+        fastNode = fastNode.next;
+        slowNode = slowNode.next;
+    }
+    slowNode.next = slowNode.next != null ? slowNode.next.next : null;
+    return head;
+};
+
+
 // Runtime: 209 ms, faster than 5.16% of TypeScript online submissions for Remove Nth Node From End of List.
 // Memory Usage: 48.7 MB, less than 8.51% of TypeScript online submissions for Remove Nth Node From End of List.
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
