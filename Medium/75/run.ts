f5d70259c1038af9569c0e02f51dba861931f21d
Ray <rayflyworld@gmail.com>
2022-10-04 21:58:47 +0900

    add : 75

---
+++ Medium/75/run.ts
@@ -0,0 +1,34 @@
+// Runtime: 141 ms, faster than 5.56% of TypeScript online submissions for Sort Colors.
+// Memory Usage: 42.5 MB, less than 95.06% of TypeScript online submissions for Sort Colors.
+function sortColors(nums: number[]): void {
+    let count = 1;
+    while (count < nums.length) {
+        for (let i = 0; i < nums.length-count; i++) {
+            if (nums[i] > nums[i+1]) {
+                let temp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = temp;
+            }
+        }
+        count++;
+    }
+};
+
+
+// Runtime: 98 ms, faster than 60.18% of TypeScript online submissions for Sort Colors.
+// Memory Usage: 42.7 MB, less than 91.05% of TypeScript online submissions for Sort Colors.
+function sortColors2(nums: number[]): void {
+    let hashMap = new Map([
+        [0, 0],
+        [1, 0],
+        [2, 0],
+    ]);
+    for (let i = 0; i < nums.length; i++) {
+        hashMap.set(nums[i], hashMap.get(nums[i])+1);
+    }
+    for (let i = 0; i < nums.length; i++) {
+        const assignVal = hashMap.get(0) > 0 ? 0 : hashMap.get(1) > 0 ? 1 : 2;
+        nums[i] = assignVal;
+        hashMap.set(assignVal, hashMap.get(assignVal)-1);
+    }
+};
