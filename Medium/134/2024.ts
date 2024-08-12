9e6cace20055b08ee23b95df0e2b923d018800cc
Ray <rayflyworld@gmail.com>
2024-08-12 13:56:21 +0900

    134

--- Medium/134/2024.ts
+++ Medium/134/2024.ts
@@ -24,9 +24,17 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
 
 
 
-// sum of gas >= sum of cost
-// [ - ]  [ + ]
-/*
+/* 
+ * 思路。
+ * 1. Gas的總量比Cost的總量還大或等於的話，就一定存在一個答案 sum of gas >= sum of cost，題目也有提到，如果有解的話，解是唯一的
+ * 2. 從index開始時，或index本身的gas - cost < 0的話，就代表這個index不可能是答案
+ * 3. 在找的時候的想法是，從0 index開始去尋找，如果計算到 x index時，方向剩餘瓦斯量是負數的
+ * 則代表代表0 ~ x index都不可能是答案。因為0 index的剩餘瓦斯量是正數，我們才會去計算。
+ * 而少了0 index，從1 index開始到x的話，少了一個正數，所以+數的瓦斯量會更少。所以我們就可以得知，0 ~ x index都不可能是答案
+ * 所以接著我們就可以從x+1 index開始計算。
+ * 4. 另外，我們只需要計畫x+1到最後一個index的瓦斯是否足夠即可。
+ * 因為你可以想像[0~x]是一個負數，而[x+1~n]是一個正數，而我們的前提是sum of gas >= sum of cost，所以[x+1~n]是正數的話，則代表x+1 index是答案
+ *
  * Reference: https://www.youtube.com/watch?v=lJwbPZGo05A
  * Runtime 76 ms Beats 82.02%
  * Memory 61.57 MB Beats 34.96%
