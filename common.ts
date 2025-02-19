1ec84a6cbf194e73575317995958f098ef870731
Ray <rayflyworld@gmail.com>
2025-02-19 11:52:58 +0900

    Returns the number of digits in a given number without converting it to a string.

--- common.ts
+++ common.ts
@@ -24,3 +24,18 @@ function isPalindrome(s: string): boolean {
 function convertSetToArray(set: Set<number>) {
     return Array.from(set);
 }
+
+/**
+ * Returns the number of digits in a given number without converting it to a string.
+ * @param number - The number to check.
+ * @returns The number of digits in the number.
+ */
+function getDigitCount(number: number): number {
+    let digitCount = 0;
+    if (number === 0) return 1;
+    while (number >= 1) {
+        number = Math.floor(number / 10);
+        digitCount++;
+    }
+    return digitCount;
+}
