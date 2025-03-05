1ff332b00144daf2cc65e8bd798394ed43004c50
Ray <rayflyworld@gmail.com>
2025-03-05 11:32:15 +0900

    update 39

--- Medium/39/2025_s1.ts
+++ Medium/39/2025_s1.ts
@@ -1,6 +1,6 @@
 /*
- * Runtime 10 ms Beats 24.92%
- * Memory 64.69 MB Beats 5.98%
+ * Runtime 3 ms Beats 68.88%
+ * Memory 63.25 MB Beats 7.75%
  */
 function combinationSum(candidates: number[], target: number): number[][] {
     const answer: Set<number[]> = new Set();
@@ -11,12 +11,12 @@ function combinationSum(candidates: number[], target: number): number[][] {
 };
 
 function calSum(startIndex: number, candidates: number[], target: number, answerItem: number[], answer: Set<number[]>) {
-    if (target < 0) return;
     if (target === 0) {
         answer.add(answerItem);
         return;
     }
     for (let i = startIndex; i < candidates.length; i++) {
+        if (target-candidates[i] < 0) continue;
         calSum(i, candidates, target-candidates[i], [...answerItem, candidates[i]], answer)
     }
     return;
