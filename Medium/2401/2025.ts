880919fc9d0d1df326d4d3ec1afd05e0ef4bce9e
Ray <rayflyworld@gmail.com>
2025-03-18 15:37:18 +0900

    optimize code 2401

--- Medium/2401/2025.ts
+++ Medium/2401/2025.ts
@@ -1,6 +1,6 @@
 /* 
- * Runtime 1175 ms Beats 7.69%
- * Memory 72.86 MB Beats 7.69%
+ * Runtime 1163 ms Beats 7.69%
+ * Memory 72.53 MB Beats 7.69%
  */
 function longestNiceSubarray(nums: number[]): number {
     const hashMap: Map<number, boolean> = new Map<number, boolean>();
@@ -8,8 +8,7 @@ function longestNiceSubarray(nums: number[]): number {
     let right = 0;
     let result = 1;
     while(right < nums.length) {
-        const binaryString = convertNumToBinary(nums[right])
-        const oneArray = convertStringToPosition(binaryString);
+        const oneArray = convertNumToBinaryPosition(nums[right]);
         let isOKAdd = true
         for (let i = 0; i < oneArray.length; i++) {
             if (hashMap.has(oneArray[i])) {
@@ -24,19 +23,18 @@ function longestNiceSubarray(nums: number[]): number {
                 hashMap.set(oneArray[i], true);
             }
             continue;
-        } else {
-            const binaryStringLeft = convertNumToBinary(nums[left])
-            const oneArrayLeft = convertStringToPosition(binaryStringLeft);
-            for (let i = 0; i < oneArrayLeft.length; i++) {
-                hashMap.delete(oneArrayLeft[i])
-            }
-            left++            
         }
+        const oneArrayLeft = convertNumToBinaryPosition(nums[left])
+        for (let i = 0; i < oneArrayLeft.length; i++) {
+            hashMap.delete(oneArrayLeft[i])
+        }
+        left++            
         
     }
     return result;
 }
-function convertStringToPosition(str: string): number[] {
+function convertNumToBinaryPosition(num: number): number[] {
+    const str = num.toString(2);
     const result: number[] = []
     for (let i = str.length-1; i >= 0; i--) {
         if (str[i] === "1") {
@@ -44,8 +42,4 @@ function convertStringToPosition(str: string): number[] {
         }
     }
     return result;
-}
-  
-function convertNumToBinary(num: number): string {
-    return num.toString(2);
 }
